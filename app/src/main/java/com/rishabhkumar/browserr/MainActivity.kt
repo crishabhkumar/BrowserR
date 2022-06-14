729047d987cb0193017c45bc8f9ad172c54c6438
Rishabh Kumar <crishabhkumar@gmail.com>
2022-06-14 18:32:07 +0530

    ViewBinding initialization and custom color addition.

--- app/src/main/java/com/rishabhkumar/browserr/MainActivity.kt
+++ app/src/main/java/com/rishabhkumar/browserr/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rishabhkumar.browserr
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.rishabhkumar.browserr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
